659c76eb1096349464eda461abc04bf9e55ac6cb
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-07-19 10:36:13 +0200

    Escape Elements

---
+++ ultimate_javascript/index.js
@@ -0,0 +1,10 @@
+//Javascript Algorithm
+
+//Escape element
+console.log("I'm the main man");
+console.log("Line number 1\n line number 2");
+console.log("Demostrating tab \t space");
+console.log("Carrige return \t move");
+console.log("word for \b boundary \b");
+console.log("form feed \f feed");
+console.log("backslash\\");
