45cff2ebe3a32dbe86415e697445b8ec1a3a86ab
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-07-19 11:42:53 +0200

    multiply multidimensional array

--- ultimate_javascript/index.js
+++ ultimate_javascript/index.js
@@ -50,4 +50,17 @@ function addAll(arr) {
   }
   return sum;
 }
-console.log(addAll([1, 2, 3, 4, 4]));
+addAll([1, 2, 3, 4, 4]);
+
+//Product of multidimensional Array
+function multiplyAll(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+
+multiplyAll([1, 2, 3, 4]);
