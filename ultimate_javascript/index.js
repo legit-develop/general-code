bd23ad4ceef362c74120fd31af285115498e0147
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-07-27 12:26:05 +0200

    bird constructor prototype set manually

--- ultimate_javascript/index.js
+++ ultimate_javascript/index.js
@@ -752,4 +752,15 @@ duck.constructor === Object  //true
 duck instance of Bird        //true
 
 //Whenever a prototype is set manually also define the constructor property
+ Bird.prototype = {
+  constructor: Bird,
+  numLegs: 2,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe:function() {
+    console.log("My name is " + this.name);
+  }
+ }
 
+ 
