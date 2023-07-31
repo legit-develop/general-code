4a05fb2ac7ae84a03194c5520b69932442147b9b
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-07-31 04:49:52 +0200

    functional programming

---
+++ ultimate_javascript_2/index.js
@@ -0,0 +1,4 @@
+//functional programming
+//Isolated functions = there are no dependance on the state of the program, which includes global variables that are subject to change
+//Pure functions - the same input always gives the same output
+//functions with limited side effects - any changes or mutation to the state of the program outside of the function are carefully controlled.
