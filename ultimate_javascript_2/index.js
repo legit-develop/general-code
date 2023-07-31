d359aebd4c8f0adddbd614519da924b45cb877e7
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-07-31 17:10:55 +0200

    side notes

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -2,3 +2,10 @@
 //Isolated functions = there are no dependance on the state of the program, which includes global variables that are subject to change
 //Pure functions - the same input always gives the same output
 //functions with limited side effects - any changes or mutation to the state of the program outside of the function are carefully controlled.
+
+//Functional Programming Terminology
+// Callbacks are function that are slipped or passed into another function to decide the invocation of that function
+// Functions that can be assigned to a variable, passed into another function, or returned from another function just like normal value are called first class functions
+// All Javascript functions are first class function
+// The function that take a function as an argument, or return a function as a return value are called higher order functions
+// When functions are passed into or returned from another function, than those functions which were passed in or returned can be called lambda
