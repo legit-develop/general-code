77a0cc03a5464216559be083dbcc95c8ad1220f5
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-01 15:16:07 +0200

    prototype map method 2

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -134,6 +134,13 @@ Array.prototype.myMap = function(callback) {
   }
 }
 
-
+//Method 2
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  for(let i = 0; i < this.length; i++){
+    newArray.push(callback(this[i]), i, this);
+  }
+  return newArray;
+}
 
 
