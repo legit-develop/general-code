fa060371d27ffa3729c4805c28ad27e004985d8e
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-08 16:29:05 +0200

    nested loop for unique values

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -673,7 +673,15 @@ uniqueValue([1, 3, 2], [5, 2, 1, 4], [2, 10]);
 // Alternative Method
 function uniteUnique(arr) {
   let args = [...arguments];
-  return arr;
+  let unique = [];
+  for (let i = 0; i < args.length; i++) {
+    for (let j = 0; j < args[i].length; j++) {
+      if (unique.indexOf(args[i][j] === -1)) {
+        unique.push(args[i][j]);
+      }
+    }
+  }
+  return unique;
 }
 
 console.log(uniteUnique([1, 2, 3], [1, 5, 6, 7], [2, 6, 9]));
