6b46cba09d90c0ffc884debdcb89e32d23fa6715
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-08 16:25:58 +0200

    set united unique values

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -668,4 +668,12 @@ function uniqueValue(arr) {
   return uniqueValueArray;
 }
 
-console.log(uniqueValue([1, 3, 2], [5, 2, 1, 4], [2, 10]));
+uniqueValue([1, 3, 2], [5, 2, 1, 4], [2, 10]);
+
+// Alternative Method
+function uniteUnique(arr) {
+  let args = [...arguments];
+  return arr;
+}
+
+console.log(uniteUnique([1, 2, 3], [1, 5, 6, 7], [2, 6, 9]));
