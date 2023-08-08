c84133071559665c949456ac937a2fe3d7eda6e2
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-08 16:12:30 +0200

    Sorted Union problem statement

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -649,4 +649,8 @@ function missLetter(str) {
   }
   return undefined;
 }
-console.log(missLetter("mike"));
+missLetter("mike");
+
+// Sorted Union
+// Write a function that takes two or more arrays and returns a new array of unique
+// values in the order of the original provided array
