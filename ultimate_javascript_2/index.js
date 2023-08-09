f6c46a68f732be4f73f76def85d3bbe253657ce6
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-09 12:07:37 +0200

    set the while loop

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -786,7 +786,14 @@ subPrime(2);
 // The return the rest of the array once the condition is satisfied, otherwise the array should be returned as an empty array
 
 function dropElements(arr, func) {
+  while (arr.length > 0 && !func(arr[0])) {
+    arr.shift();
+  }
   return arr;
 }
 
-console.log(dropElements());
+console.log(
+  dropElements([1, 2, 3, 4], function (n) {
+    return n > 5;
+  })
+);
