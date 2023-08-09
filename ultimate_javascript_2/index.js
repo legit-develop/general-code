0f3a84e39c3b8f65e69b2b72e8792da4b9a19315
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-09 11:55:57 +0200

    set up the prime function

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -756,3 +756,9 @@ sumFibs(3);
 // Sum of Primes
 // A prime number is a whole number greater than 1 with exactly two divisors 1 and itself.
 // Example 2 is a prime number divided by 1 and 2
+
+function isPrime(x) {
+  if (x === 2) {
+    return true;
+  }
+}
