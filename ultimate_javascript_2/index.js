fdc16eebfe9308eb7e80e01840f2b5138a2f01e2
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-10 15:27:16 +0200

    function setup

--- ultimate_javascript_2/index.js
+++ ultimate_javascript_2/index.js
@@ -787,3 +787,9 @@ subPrime(2);
 
 // Steamroller
 // Flatten a nested array. You must account for varying levels of nesting
+
+function steamrollArray(arr) {
+  return arr;
+}
+
+steamrollArray([[1, 2, 3], 1, [3, [4]]]);
