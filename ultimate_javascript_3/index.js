8d0f9e31b7539c0cb7f7dcb87ebd8bf24676b1d1
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-12 07:01:52 +0200

    set the functin to modify the supplied string

--- ultimate_javascript_3/index.js
+++ ultimate_javascript_3/index.js
@@ -1,2 +1,9 @@
 // Palindrome Checker
 // Return true if the given string is a palindrome, otherwise return false
+
+function palindrome(str) {
+  let lowCaseStr = str.toLowerCase();
+  return lowCaseStr;
+}
+
+console.log(palindrome("SIFISO"));
