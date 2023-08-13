58d3e1077db28bf4030cd62bb0718a4f5859307f
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-13 12:00:19 +0200

    caesars cipher problem statement

--- ultimate_javascript_3/index.js
+++ ultimate_javascript_3/index.js
@@ -3,7 +3,14 @@
 
 function palindrome(str) {
   let lowCaseStr = str.toLowerCase();
-  return lowCaseStr;
+  let regexStr = /\W|_/g;
+  let letterStr = lowCaseStr.replace(regexStr, "");
+  let letterArray = [];
+  return letterStr;
 }
 
 console.log(palindrome("SIFISO"));
+
+// Caesars Cipher
+// One of the simplest and most widely know ciphers is a caesar cipher, also known as a shift cipher
+// In a shift cipher meanings of the letters are shifted by some set amount
