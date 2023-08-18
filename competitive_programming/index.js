2b9a647b760be02e2594bf5b8dee5aee1cb96e09
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-18 15:14:05 +0200

    candy division function

---
+++ competitive_programming/index.js
@@ -0,0 +1,10 @@
+// 1. Candies
+// Determine how many pieces of candy will be eaten by all the children together
+// Individual pieces of candy can not be split
+
+function childCandy(child, candy) {
+  let numberOfCandies = Math.floor(candy / child);
+  return numberOfCandies * child;
+}
+
+childCandy(3, 14);
