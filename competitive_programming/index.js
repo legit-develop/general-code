fbdae24644179d11247ddada76035d488d060c58
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-19 12:13:45 +0200

    bank deposit problem statement

--- competitive_programming/index.js
+++ competitive_programming/index.js
@@ -8,3 +8,10 @@ function childCandy(child, candy) {
 }
 
 childCandy(3, 14);
+
+console.log("sifiso dhlamini");
+
+// 2. Bank Deposit
+// You have deposited a specific amount of dollars into your bank account.
+// Each year your balance increases at the same growth rate.
+// Find out how long it would take for your balance a pass specific threshold with the assumption that you don't make any additional deposits
