ff18ae2a39d38b0e2d06aca44d7602c49d055d46
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-22 07:56:10 +0200

    count vowels

--- competitive_programming/index.js
+++ competitive_programming/index.js
@@ -79,3 +79,8 @@ function sortArray(arr) {
 }
 
 sortArray([1, 2, 5, 3, 4, 2, 8, 1]);
+
+// 6. Count Vowels Consonant
+// You are given a string s that consists of only lowercase English letter.
+// If vowels ('a', 'e', 'i', 'o', 'u') are given a value of 1 and consonants are given a value of 2
+// return the sum of all the letters in the input string.
