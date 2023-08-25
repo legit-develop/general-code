a5272275b48ca4074857006a45ab444d55fb567d
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-25 11:24:17 +0200

    adjacent return function with product integers

--- competitive_programming/index.js
+++ competitive_programming/index.js
@@ -119,3 +119,9 @@ CountVen("i");
 
 // 10. Adjacent Elements Product
 // Given an array of integers, find the pair of adjacent elements that has the largest product and return that product
+
+const adjacentElementProduct = (inputArray) => {
+  return inputArray;
+};
+
+console.log(adjacentElementProduct([1, 2, 3, 4]));
