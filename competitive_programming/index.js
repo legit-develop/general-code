a2f34cd051513aca905e79f68576b069a73294a5
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-29 16:09:24 +0200

    split the time into hours and minutes

--- competitive_programming/index.js
+++ competitive_programming/index.js
@@ -149,6 +149,7 @@ inputArray([1, 2, 3, 4, 5]);
 // Check if the given string is a correct time representation of the 24-hour clock
 
 function validTime(time) {
+  const timeSplit = time.split(":");
   return time;
 }
 
